aa95ead19548920ecd9f81057dc7fdef77ea7ee6
Kara Schuler <kschuler@mines.edu>
2024-03-08 11:14:45 -0700

    started making inventory GUI, still needs a lot of work but figured out which type of GUI is best (boxes with GUIContent)

--- Scripts/InventoryManagement.cs
+++ Scripts/InventoryManagement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InventoryManagement : MonoBehaviour
 {
+    bool ears = false;
     bool eyes = false;
     
     // Start is called before the first frame update
@@ -16,7 +17,13 @@ public class InventoryManagement : MonoBehaviour
     void Update()
     {
         
-        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+        // check for equipping ears
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            ears = !ears;
+        }
+
+        // check for equipping eyes
+        if(Input.GetKeyDown(KeyCode.Alpha2)) {
             eyes = !eyes;
         }
 
@@ -24,6 +31,7 @@ public class InventoryManagement : MonoBehaviour
 
     void OnGUI() {
 
+        // For testing //
         string message;
 
         if (eyes) {
@@ -33,6 +41,27 @@ public class InventoryManagement : MonoBehaviour
         }
 
         GUI.Label(new Rect(10, 30, 500, 20), message);
+        ////
+        
+        // draw inventory slots
+        int slot_x = 40;
+        int slot_y = 15;
+        int slot_spacing = 20;
+        int slot_size = 50;
+
+        // TODO: make a GUI content to get text and images
+        // TODO: figure out how to highlight buttons when they are equipped
+        // slot1: ears
+        if (ears) { // ears are equipped
+            GUI.Box(new Rect(slot_x, slot_y, slot_size, slot_size), "1 Ears EQUIPPED");
+        } else { // ears are not equipped
+            GUI.Box(new Rect(slot_x, slot_y, slot_size, slot_size), "1 Ears");
+        }
+        
+
+        // slot2: eyes
+        GUI.Box(new Rect(slot_x + slot_size + slot_spacing, slot_y, slot_size, slot_size), "2 Eyes");
+
 
     }
 
