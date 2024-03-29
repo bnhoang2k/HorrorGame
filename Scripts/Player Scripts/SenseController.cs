ddeadb285592665160398cf439f180d9b35809b7
bryanhoang <bryanhoang@mines.edu>
2024-03-29 17:16:40 -0600

    Adjusted sight script for more complex prefabs with children

--- Scripts/Player Scripts/SenseController.cs
+++ Scripts/Player Scripts/SenseController.cs
@@ -29,50 +29,35 @@ public class SenseController : MonoBehaviour
             directionalLight.enabled = false;
             player.GetComponentInChildren<Camera>().clearFlags = CameraClearFlags.Color;
             player.GetComponentInChildren<Camera>().backgroundColor = Color.black;
-            foreach (Describable describable in allDescribables)
-            {
-                if (describable)
-                {
-                    Renderer describableRenderer = describable.GetComponent<Renderer>();
-                    if (describableRenderer)
-                    {
-                        foreach (Material describableMaterial in describableRenderer.materials)
-                        {
-                            if (describableMaterial.HasProperty("_Wireframe_Color"))
-                            {
-                                Color wireframeColor = describableMaterial.GetColor("_Wireframe_Color");
-                                Color newColor = new Color(wireframeColor.r, wireframeColor.g, wireframeColor.b, 1.0f);
-                                describableMaterial.SetColor("_Wireframe_Color", newColor);
-                            }
-                        }
-                    }
-                }
-            }
         }
         else
         {
             directionalLight.enabled = true;
             player.GetComponentInChildren<Camera>().clearFlags = CameraClearFlags.Skybox;
-            foreach (Describable describable in allDescribables)
+        }
+        foreach (Describable describable in allDescribables)
+        {
+            SetWireFrame(describable.gameObject, isBlind);
+        }
+    }
+
+    private void SetWireFrame(GameObject obj, bool isBlind)
+    {
+        Renderer[] renderers = obj.GetComponentsInChildren<Renderer>(true);
+        foreach (Renderer describableRenderer in renderers)
+        {
+            foreach (Material describableMaterial in describableRenderer.materials)
             {
-                if (describable)
+                if (describableMaterial.HasProperty("_Wireframe_Color"))
                 {
-                    Renderer describableRenderer = describable.GetComponent<Renderer>();
-                    if (describableRenderer)
-                    {
-                        foreach (Material describableMaterial in describableRenderer.materials)
-                        {
-                            if (describableMaterial.HasProperty("_Wireframe_Color"))
-                            {
-                                Color wireframeColor = describableMaterial.GetColor("_Wireframe_Color");
-                                // Debug.Log(describable.name + " | " + describableMaterial.name + " | " + wireframeColor.ToString());
-                                Color newColor = new Color(wireframeColor.r, wireframeColor.g, wireframeColor.b, 0.0f);
-                                describableMaterial.SetColor("_Wireframe_Color", newColor);
-                            }
-                        }
-                    }
+                    Debug.Log("Material: " + describableMaterial.name + " has wireframe color property.");
+                    Color wireframeColor = describableMaterial.GetColor("_Wireframe_Color");
+                    Color newColor = new Color(wireframeColor.r, wireframeColor.g, wireframeColor.b, isBlind ? 1.0f : 0.0f);
+                    describableMaterial.SetColor("_Wireframe_Color", newColor);
+                    Debug.Log("Material: " + describableMaterial.name + " wireframe color set to: " + newColor);
                 }
             }
         }
     }
+
 }
