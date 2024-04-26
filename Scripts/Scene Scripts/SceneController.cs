f91db0627b339a905d1f111b3f6840cf0d32dfd9
Kara Schuler <kschuler@mines.edu>
2024-04-26 12:15:33 -0600

    Created instruction/title scene with working buttons, updated build settings so that all needed scenes are added so that the scenes can actually switch (I'm not sure if the build settings transfer through git though)

--- Scripts/Scene Scripts/SceneController.cs
+++ Scripts/Scene Scripts/SceneController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public void RestartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("House");
     }
